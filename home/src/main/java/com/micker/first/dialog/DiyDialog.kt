358f43635f11342a11cab7d78d3d053e5a1e5c18
richzjc <richzjc130109@gmail.com>
2022-10-19 20:18:12 +0800

    添加了dialog

--- home/src/main/java/com/micker/first/dialog/DiyDialog.kt
+++ home/src/main/java/com/micker/first/dialog/DiyDialog.kt
@@ -1,8 +1,37 @@
 package com.micker.first.dialog
 
+import android.view.Gravity
+import android.view.WindowManager
 import com.micker.core.base.BaseDialogFragment
 import com.micker.first.callback.DiyCallback
+import com.micker.helper.system.ScreenUtils
+import com.micker.home.R
 
 class DiyDialog : BaseDialogFragment() {
     var diyCallback: DiyCallback? = null
+
+
+    override fun getGravity(): Int {
+        return Gravity.CENTER
+    }
+
+    override fun getStyle(): Int {
+        return R.style.DefaultDialog
+    }
+
+
+    override fun getDialogWidth(): Int {
+        return ScreenUtils.getScreenWidth() - ScreenUtils.dip2px(100f)
+    }
+
+    override fun getDialogHeight(): Int {
+        return WindowManager.LayoutParams.WRAP_CONTENT
+    }
+
+    override fun doGetContentViewId() = R.layout.aqhy_dialog_first_stage_diy
+
+    override fun doInitData() {
+        super.doInitData()
+
+    }
 }
--- home/src/main/java/com/micker/first/dialog/NanduDialog.kt
+++ home/src/main/java/com/micker/first/dialog/NanduDialog.kt
@@ -1,9 +1,36 @@
 package com.micker.first.dialog
 
+import android.view.Gravity
+import android.view.WindowManager
 import com.micker.core.base.BaseDialogFragment
 import com.micker.first.callback.NanduCallback
+import com.micker.helper.system.ScreenUtils
+import com.micker.home.R
 
 class NanduDialog : BaseDialogFragment() {
 
     var nanduCallback: NanduCallback? = null
+
+    override fun getGravity(): Int {
+        return Gravity.CENTER
+    }
+
+    override fun getStyle(): Int {
+        return R.style.DefaultDialog
+    }
+
+    override fun getDialogWidth(): Int {
+        return ScreenUtils.getScreenWidth() - ScreenUtils.dip2px(100f)
+    }
+
+    override fun getDialogHeight(): Int {
+        return WindowManager.LayoutParams.WRAP_CONTENT
+    }
+
+    override fun doGetContentViewId() = R.layout.aqhy_dialog_first_stage_nandu
+
+    override fun doInitData() {
+        super.doInitData()
+
+    }
 }
