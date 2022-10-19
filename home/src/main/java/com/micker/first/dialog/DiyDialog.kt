d6618c44db966ff5b97be6fb49048dc4c075888b
richzjc <richzjc130109@gmail.com>
2022-10-19 20:43:33 +0800

    添加了dialog

--- home/src/main/java/com/micker/first/dialog/DiyDialog.kt
+++ home/src/main/java/com/micker/first/dialog/DiyDialog.kt
@@ -1,13 +1,23 @@
 package com.micker.first.dialog
 
 import android.view.Gravity
+import android.view.View
 import android.view.WindowManager
 import com.micker.core.base.BaseDialogFragment
 import com.micker.first.callback.DiyCallback
+import com.micker.global.util.ShapeDrawable
+import com.micker.helper.ResourceUtils
 import com.micker.helper.system.ScreenUtils
 import com.micker.home.R
+import kotlinx.android.synthetic.main.aqhy_dialog_first_stage_diy.*
 
 class DiyDialog : BaseDialogFragment() {
+
+    private val drawable by lazy {
+        val color = ResourceUtils.getColor(R.color.color_1482f0)
+        ShapeDrawable.getDrawable(0, ScreenUtils.dip2px(5f), color, color)
+    }
+
     var diyCallback: DiyCallback? = null
 
 
@@ -21,7 +31,7 @@ class DiyDialog : BaseDialogFragment() {
 
 
     override fun getDialogWidth(): Int {
-        return ScreenUtils.getScreenWidth() - ScreenUtils.dip2px(100f)
+        return ScreenUtils.getScreenWidth() - ScreenUtils.dip2px(50f)
     }
 
     override fun getDialogHeight(): Int {
@@ -30,8 +40,16 @@ class DiyDialog : BaseDialogFragment() {
 
     override fun doGetContentViewId() = R.layout.aqhy_dialog_first_stage_diy
 
+
     override fun doInitData() {
         super.doInitData()
+        confirm?.background = drawable
+        setListener()
+    }
 
+    private fun setListener(){
+        confirm?.setOnClickListener {
+
+        }
     }
 }
--- home/src/main/java/com/micker/first/dialog/NanduDialog.kt
+++ home/src/main/java/com/micker/first/dialog/NanduDialog.kt
@@ -4,10 +4,18 @@ import android.view.Gravity
 import android.view.WindowManager
 import com.micker.core.base.BaseDialogFragment
 import com.micker.first.callback.NanduCallback
+import com.micker.global.util.ShapeDrawable
+import com.micker.helper.ResourceUtils
 import com.micker.helper.system.ScreenUtils
 import com.micker.home.R
+import kotlinx.android.synthetic.main.aqhy_dialog_first_stage_diy.*
 
 class NanduDialog : BaseDialogFragment() {
+
+    private val drawable by lazy {
+        val color = ResourceUtils.getColor(R.color.color_1482f0)
+        ShapeDrawable.getDrawable(0, ScreenUtils.dip2px(5f), color, color)
+    }
 
     var nanduCallback: NanduCallback? = null
 
@@ -20,7 +28,7 @@ class NanduDialog : BaseDialogFragment() {
     }
 
     override fun getDialogWidth(): Int {
-        return ScreenUtils.getScreenWidth() - ScreenUtils.dip2px(100f)
+        return ScreenUtils.getScreenWidth() - ScreenUtils.dip2px(50f)
     }
 
     override fun getDialogHeight(): Int {
@@ -31,6 +39,13 @@ class NanduDialog : BaseDialogFragment() {
 
     override fun doInitData() {
         super.doInitData()
+        confirm?.background = drawable
+        setListener()
+    }
 
+    private fun setListener(){
+        confirm?.setOnClickListener {
+
+        }
     }
 }
