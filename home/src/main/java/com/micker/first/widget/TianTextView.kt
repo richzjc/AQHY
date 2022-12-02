04a74152b9c2cf0877755858de0a011146673300
richzjc <richzjc130109@gmail.com>
2022-12-02 15:17:09 +0800

    添加了TianTextView

---
+++ home/src/main/java/com/micker/first/widget/TianTextView.kt
@@ -0,0 +1,32 @@
+package com.micker.first.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.micker.helper.ResourceUtils
+import com.micker.helper.system.ScreenUtils
+import com.micker.home.R
+
+class TianTextView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatTextView(context, attrs) {
+
+    private var paint: Paint? = null
+
+    init {
+        paint = Paint()
+        paint?.isAntiAlias = true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        paint?.strokeWidth = ScreenUtils.dip2px(1f).toFloat()
+        paint?.color = ResourceUtils.getColor(R.color.day_mode_text_color1_333333)
+        val rect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
+        canvas?.drawRect(rect, paint!!)
+
+        super.onDraw(canvas)
+    }
+}
